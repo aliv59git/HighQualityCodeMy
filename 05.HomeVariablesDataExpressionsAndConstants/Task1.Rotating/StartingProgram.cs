4453d1cad3c02e148c9bca5bdf32c8e9ba4fe334
aliv59git <vilavaln@gmail.com>
2015-07-10 14:41:17 +0300

    new home5

---
+++ 05.HomeVariablesDataExpressionsAndConstants/Task1.Rotating/StartingProgram.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Figure
+{
+    public class Start
+    {
+        public static void Main()
+        {
+            Figure firstFigure = new Figure(5, 10.1);
+            Figure rotatedFirstFigure = firstFigure.GetRotatedFigure(56.4);
+        }
+    }
+}
