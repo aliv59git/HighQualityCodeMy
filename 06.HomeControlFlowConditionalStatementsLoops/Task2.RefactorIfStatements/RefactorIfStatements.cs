5ba9534a4f840891ef5e5a662b188c98ca8c80e5
aliv59 <vilavaln@gmail.com>
2015-07-23 14:22:54 +0300

    new vvvwer

---
+++ 06.HomeControlFlowConditionalStatementsLoops/Task2.RefactorIfStatements/RefactorIfStatements.cs
@@ -0,0 +1,59 @@
+﻿namespace RefactorIfStatements
+{
+    using Cooking.ToolsAndIngredients;
+
+
+    public class RefactorIfStatements
+    {
+        public static void Main()
+        {
+            /*
+            Potato potato;
+            //...
+            if (potato != null)
+            if(!potato.HasNotBeenPeeled && !potato.IsRotten)
+            Cook(potato);
+            */
+            Potato potato = new Potato();
+
+            // ....
+            if (potato != null)
+            {
+                bool notPeeled = !potato.IsPeeled;
+                bool notRotten = !potato.IsRotten;
+
+                if (notPeeled && notRotten)
+                {
+                    potato.Cook();
+                }
+            }
+
+            const int MinX = 5;
+            const int MaxX = 15;
+            const int MinY = 5;
+            const int MaxY = 15;
+
+            int x = 7;
+            int y = 8;
+
+            bool shouldVisitCell = true;
+
+            if (shouldVisitCell && IsInRange(x, MinX, MaxX) && IsInRange(y, MinY, MaxY))
+            {
+                VisitCell();
+            }
+        }
+
+        public static void VisitCell()
+        {
+            System.Console.WriteLine("Visited");
+        }
+
+        public static bool IsInRange(int value, int min, int max)
+        {
+            bool isInRange = value >= min && value <= max;
+
+            return isInRange;
+        }
+    }
+}
