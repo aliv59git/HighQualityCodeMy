f989a1f5af1403f36e543aa080270be5d3c3488c
aliv59 <vilavaln@gmail.com>
2015-08-14 23:27:58 +0300

    home 13

---
+++ 13HomeRefactoring/Matrix/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Matrix
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            var m = new Matrix(10);
+            Console.WriteLine(m);
+        }
+    }
+}
